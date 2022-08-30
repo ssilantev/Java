b6e111d2afb1408be5465a8e54b7737d17327889
ssilantev <un.realgar@yandex.ru>
2022-08-30 18:27:04 +0700

    Homework #4: Average undone

---
+++ src/ru.academIt.java/Average/Action.java
@@ -0,0 +1,109 @@
+package Average;
+
+import java.time.YearMonth;
+
+public class Action {
+	public class Range {
+		private String a;
+		private String b;
+
+		public Range(String a, String b) {
+			this.a = a;
+			this.b = b;
+		}
+	}
+
+	public static boolean CheckInt(String a, String b) { // Сюда нужно подставить a или b, как правильно?
+		for () {
+			try {
+				int intValueA = Integer.parseInt(a);
+				return true;
+			} catch (NumberFormatException e) {
+				System.out.println("Incorrect data type");
+			}
+			return false;
+		}
+	}
+
+	public static int IntForm(String a,String b) { // Сюда нужно подставить a или b, как правильно?
+		int intValue = Integer.parseInt();
+		return intValue;
+		}
+	}
+
+
+
+//	public static boolean IntA(String a) {
+//
+//		if (a == null || a.equals("")) {
+//			System.out.println("Empty data.");
+//			return false;
+//		}
+//		try {
+//			int intValueA = Integer.parseInt(a);
+//			return true;
+//		} catch (NumberFormatException e) {
+//			System.out.println("Incorrect data type");
+//		}
+//		return false;
+//	}
+//	public static boolean IntB(String b) {
+//		int intValueA;
+//
+//		if (b == null || b.equals("")) {
+//			System.out.println("Empty data.");
+//			return false;
+//		}
+//		try {
+//			intValueA = Integer.parseInt(b);
+//			return true;
+//		} catch (NumberFormatException e) {
+//			System.out.println("Incorrect data type");
+//		}
+//		return false;
+//	}
+}
+
+
+
+//	public static boolean isInt(String a, String b) {
+//		//		System.out.println(String.format("Parsing string: \"%s\"", ));
+//
+//		for (String var : String a, String) b)
+//			 {
+//
+//		}
+//
+//		if ((a.length() == 0) || (b.length() == 0)){
+//			System.out.println("Empty data!");
+//			return false;
+//		}
+//		else{
+//			try {
+//				int intValue = Integer.parseInt(a);
+//				System.out.println("Int type confirmed.");
+//				return true;
+//			} catch (NumberFormatException e) {
+//				System.out.println("Input String cannot be parsed to Integer.");
+//
+//				return false;
+//			}
+//
+//		if (b.length() == 0) {
+//			System.out.println("Empty data!");
+//			return false;
+//		}
+//		else{
+//			try {
+//				int intValue = Integer.parseInt(b);
+//				System.out.println("Int type confirmed.");
+//				return true;
+//			} catch (NumberFormatException e) {
+//				System.out.println("Input String cannot be parsed to Integer.");
+//
+//				return false;
+//			}
+//
+//		}
+//	}
+//}
--- src/ru.academIt.java/Average/MainAverage.java
+++ src/ru.academIt.java/Average/MainAverage.java
@@ -2,67 +2,28 @@ package Average;
 
 import java.util.Scanner;
 
-import static Average.Average.isNumeric;
-
 public class MainAverage {
 	public static void main(String[] args) {
-		System.out.print("Найдем среднее арифметическое всех (и отдельно - только четных) цлых чисел от a до b.\n");
+		System.out.print("Find average for all and specialy all even int numbers from a to b.\n");
 		Scanner scanner = new Scanner(System.in);
 		String a = "";
-//		int intA = Integer.parseInt(a);
-		while (isNumeric(a) = false) {
-			System.out.print("Задайте значение a: ");
+		while (a.CheckInt(false) || (IntFormA < 2)) {
+			System.out.print("Type a: ");
 			a = scanner.nextLine();
-//			{
-				if (isNumeric(a)){
-					System.out.print("Вы ввели: " + a +".");
-				}
-//			}
+			if (a.length() > 0) {
+				System.out.print("a is correct.");
+			}
 		}
+		String b = "";
+		while (b.CheckInt(false) || (a.IntForm >= b.IntForm)) {
+			System.out.print("Type b: ");
+			a = scanner.nextLine();
+			if (a.length() > 0) {
+				System.out.print("b is correct.");
+			}
+		}
+//			Here is average for all int numbers from a to b.;
+
+//			Here is average for only even int numbers from a to b.;
 	}
 }
-
-//		Scanner scanner = new Scanner(System.in);
-//		int a = scanner.nextInt();
-//		String strA = Integer.toString(a);
-//		while (strA.length() == 0) {
-//			System.out.print("Введите значение a: ");
-//			a = scanner.nextInt();
-//			if (a < 2) {
-//				System.out.print("Ошика, введите заного");
-//			} else {
-//				System.out.print("Программа завершена");
-//			}
-//		}
-//	}
-//}
-
-//		Scanner scanner = new Scanner(System.in);
-//		int a = scanner.nextLine();
-//		while (a.equals() = 0) {
-//			System.out.print("Введите значение a, которое является целым числом и больше \"1\": ");
-//			a = scanner.nextLine();
-//			if (a.length() > 0) {
-//				if (a < 2) {
-//					System.out.print("Введите корректное значение a: ");
-//					a = scanner.nextLine();
-//				} else {
-//					System.out.print(a);
-//				}
-//			}
-//		}
-
-
-//		System.out.print("Введите значение b: ");
-//		int b = scanner.nextLine();
-
-
-//	String name = "";
-//		while (name.length() == 0) {
-//				System.out.print("Задайте значение a: ");
-//				name = scanner.nextLine();
-//				if (name.length() > 0) {
-//				System.out.print("Привет, " + name + "!");
-//				}
-//				}
-//				}}
