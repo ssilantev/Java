45edec76e753c825aea65e7ca8c33926a137afed
ssilantev <un.realgar@yandex.ru>
2022-09-01 16:38:57 +0700

    Homework #4: Average - ready

--- src/ru.academIt.java/Average/Action.java
+++ src/ru.academIt.java/Average/Action.java
@@ -1,106 +1,60 @@
 package Average;
 
 public class Action {
-//	public class Range {
-//		private String a;
-//		private String b;
-//
-//		public Range(String a, String b) {
-//			this.a = a;
-//			this.b = b;
-//		}
-//	}
-
-//	public static boolean CheckInt(String str) { // Сюда нужно подставить a или b, как правильно?
-//		for (str) {
-//			try {
-//				int intValueA = Integer.parseInt(str);
-//				return true;
-//			} catch (NumberFormatException e) {
-//				System.out.println("Incorrect data type");
-//			}
-//			return false;
-//		}
-//	}
+	public static boolean IfInt(String s) { // Сюда нужно подставить a или b, как правильно?
+		try {
+			int testInt = Integer.parseInt(s);
+			return true;
+		} catch (NumberFormatException e) {
+			System.out.println("Not int!");
+		}
+		return false;
+	}
 
 	public static int IntValue(String s) {
 		return Integer.parseInt(s);
 	}
+
+	public static double Average(int aInt, int bInt) {
+
+		System.out.print("\nThe array means are: ");
+		for (int i = aInt; i <= bInt; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+
+		int count = 0;
+		double sum = 0.0;
+		while (aInt <= bInt) {
+			sum = sum + aInt;
+			count++;
+			aInt++;
+		}
+		System.out.println("Sum = " + sum);
+		System.out.println("Total Numbers = " + count);
+		return (sum / count);
+	}
+
+	public static double AverageEven(int aInt, int bInt) {
+		System.out.print("\nThe array EVEN means are: ");
+		for (int i = aInt; i <= bInt; i++) {
+			if (i % 2 == 0) {
+				System.out.print(i + " ");
+			}
+		}
+		System.out.println();
+
+		int count = 0;
+		double sum = 0.0;
+		while (aInt <= bInt) {
+			if (aInt % 2 == 0) {
+				sum = sum + aInt;
+				count++;
+			}
+			aInt++;
+		}
+		System.out.println("Sum (EVEN) = " + sum);
+		System.out.println("Total Numbers (EVEN): = " + count);
+		return (sum / count);
+	}
 }
-
-
-
-//	public static boolean IntA(String a) {
-//
-//		if (a == null || a.equals("")) {
-//			System.out.println("Empty data.");
-//			return false;
-//		}
-//		try {
-//			int intValueA = Integer.parseInt(a);
-//			return true;
-//		} catch (NumberFormatException e) {
-//			System.out.println("Incorrect data type");
-//		}
-//		return false;
-//	}
-//	public static boolean IntB(String b) {
-//		int intValueA;
-//
-//		if (b == null || b.equals("")) {
-//			System.out.println("Empty data.");
-//			return false;
-//		}
-//		try {
-//			intValueA = Integer.parseInt(b);
-//			return true;
-//		} catch (NumberFormatException e) {
-//			System.out.println("Incorrect data type");
-//		}
-//		return false;
-//	}
-
-
-
-
-//	public static boolean isInt(String a, String b) {
-//		//		System.out.println(String.format("Parsing string: \"%s\"", ));
-//
-//		for (String var : String a, String) b)
-//			 {
-//
-//		}
-//
-//		if ((a.length() == 0) || (b.length() == 0)){
-//			System.out.println("Empty data!");
-//			return false;
-//		}
-//		else{
-//			try {
-//				int intValue = Integer.parseInt(a);
-//				System.out.println("Int type confirmed.");
-//				return true;
-//			} catch (NumberFormatException e) {
-//				System.out.println("Input String cannot be parsed to Integer.");
-//
-//				return false;
-//			}
-//
-//		if (b.length() == 0) {
-//			System.out.println("Empty data!");
-//			return false;
-//		}
-//		else{
-//			try {
-//				int intValue = Integer.parseInt(b);
-//				System.out.println("Int type confirmed.");
-//				return true;
-//			} catch (NumberFormatException e) {
-//				System.out.println("Input String cannot be parsed to Integer.");
-//
-//				return false;
-//			}
-//
-//		}
-//	}
-//}
--- src/ru.academIt.java/Average/MainAverage.java
+++ src/ru.academIt.java/Average/MainAverage.java
@@ -4,27 +4,26 @@ import java.util.Scanner;
 
 public class MainAverage {
 	public static void main(String[] args) {
-		System.out.println("Find average for all and specialy all even int numbers from a to b.");
+		System.out.println("Find average for all and specially all even int numbers from a to b.");
 		Scanner scanner = new Scanner(System.in);
 		String a = "";
-		while (a.length() == 0){
-//		while ((Action.CheckInt(a) = false) || (Action.IntForm(a) < 2)) {
-			System.out.print("Type a: ");
+		while ((a.length() == 0) || !(Action.IfInt(a)) || (Action.IntValue(a) < 2)) {
+			System.out.print("Type \"a\" greater than \"1\": ");
 			a = scanner.nextLine();
 		}
 		String b = "";
-		while ((b.length() == 0) || (Action.IntValue(a) <= Action.IntValue(b))) {
-			System.out.println("Type b: ");
+		while ((b.length() == 0) || !(Action.IfInt(a)) || (Action.IntValue(b) <= Action.IntValue(a))) {
+			System.out.print("Type \"b\" greater than \"a\": ");
 			b = scanner.nextLine();
-			if (b.length() > 0) {
-				System.out.println("b is correct.");
-			}
 		}
-//			Here is average for all int numbers from a to b.;
 
-//			Here is average for only even int numbers from a to b.;
+		int aInt = Action.IntValue(a);
+		int bInt = Action.IntValue(b);
+
+		System.out.println("Values received: a = " + a + ", b = " + b + ".");
+
+		System.out.println("The average mean of all array members from a to b is: " + Action.Average(aInt, bInt) + ".");
+
+		System.out.println("The average mean of EVEN array members from a to b is: " + Action.AverageEven(aInt, bInt) + ".");
 	}
 }
-
-// while ((Action.CheckInt(a) = false) || (Action.IntForm(a) < 2)) {
-// 		while ((Action.CheckInt(b) = false) || (Action.IntForm(a) <= Action.IntForm(b))) {
