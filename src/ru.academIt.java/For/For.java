3765dbb2ce3e43e299c1dc7a5f9fbcef5043147a
ssilantev <un.realgar@yandex.ru>
2022-09-02 17:24:10 +0700

    Homework #4: For, undone

--- src/ru.academIt.java/For/For.java
+++ src/ru.academIt.java/For/For.java
@@ -1,8 +1,11 @@
-//package For;
-//
-//public class For {
-//	public static int dividedIntoFour(){
-//
-//
-//	}
-//}
+package For;
+
+public class For {
+	public static void dividedIntoFour(int[] s){
+		for (int j = i; j >= 1; j--) {
+			if (j % 4 == 0) {
+				System.out.print(j + " ");
+			}
+		}
+	}
+}
--- src/ru.academIt.java/For/MainFor.java
+++ src/ru.academIt.java/For/MainFor.java
@@ -2,24 +2,13 @@ package For;
 
 public class MainFor {
 	public static void main(String[] args) {
-		int[] Array = new int[101];
-		for (int i = 100; i >= 1; i--) {
+		int[] Array = new int[51];
+		for (int i = 50; i >= 1; i--) {
 			Array[i] = i;
-			System.out.print(Array[i] + " ");
+			System.out.print(i + " ");
 		}
+		System.out.println();
+		For.dividedIntoFour(Array);
 	}
 
-
-
-
-//	for (int i = 100; i > 1; i--) {
-//		System.out.print(i + " ");
-
-
-//	int[] thirteenMultiples = new int[400];
-//    for (int dex = 0; dex < thirteenMultiples.length; dex ++) {
-//		int multiples = 13 * dex;
-//		thirteenMultiples[dex] = multiples;
-//		System.out.println(Arrays.toString(thirteenMultiples));
-//	}
 }
