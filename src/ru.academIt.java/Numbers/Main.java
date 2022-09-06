9d12bb1556d058beef6fa600aa631efbe348db9d
ssilantev <un.realgar@yandex.ru>
2022-09-06 22:25:10 +0700

    Homework #4: Numbers, ready

--- src/ru.academIt.java/Numbers/Main.java
+++ src/ru.academIt.java/Numbers/Main.java
@@ -6,17 +6,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String input = "";
-		while ((input.length() == 0) || !(Methods.ifInt(input)) || (Methods.intValue(input) < 1)) {
+		while ((input.length() == 0) || !(Methods.ifLong(input)) || (Methods.intValue(input) < 1)) {
 			System.out.print("Enter natural value: ");
 			input = scanner.nextLine();
-
 		}
-		System.out.println("Input is: " + input);
 
 		System.out.println("Sum of all numbers is: " + Methods.sumNumbers(input));
 
-//		System.out.println("Sum of even numbers is: " + Methods.sumEvenNumbers(input));
+		System.out.println("Sum of even numbers is: " + Methods.sumEvenNumbers(input));
 
-//		System.out.println("Greatest number is: " + Methods.greatestNumber(input));
+		System.out.println("Greatest number is: " + Methods.greatestNumber(input));
 	}
 }
--- src/ru.academIt.java/Numbers/Methods.java
+++ src/ru.academIt.java/Numbers/Methods.java
@@ -1,26 +1,54 @@
 package Numbers;
 
 public class Methods {
-	public static boolean ifInt(String s) {
+	public static boolean ifLong(String s) {
+//	public static boolean ifInt(String s) {
 		try {
-			int testInt = Integer.parseInt(s);
+			long testLong = Long.parseLong(s, 10);
+//			int testInt = Integer.parseInt(s);
 			return true;
 		} catch (NumberFormatException e) {
-//			System.out.println("Not int!");
+			System.out.println("Not int!");
 		}
 		return false;
 	}
 
-	public static int intValue(String s) {
-		return Integer.parseInt(s);
+	public static long intValue(String s) {
+		return Long.parseLong(s, 10);
 	}
-	public static int sumNumbers(String s){
-		int num = intValue(s);
-		int sum = 0;
-		while(num != 0){
+
+	public static long sumNumbers(String s) {
+		long num = intValue(s);
+		long sum = 0;
+		while (num > 0) {
 			sum += (num % 10);
 			num /= 10;
 		}
 		return sum;
+	}
+
+	public static long sumEvenNumbers(String s) {
+		long numEven = intValue(s);
+		long sumEven = 0;
+		while (numEven > 0) {
+			if (numEven % 2 == 0) {
+				sumEven += (numEven % 10);
+			}
+			numEven /= 10;
+		}
+		return sumEven;
+	}
+
+	public static long greatestNumber(String s) {
+		long n = intValue(s);
+		int a, m = 0;
+		while (n > 0) {
+			a = (int) n % 10;
+			if (a > m) {
+				m = a;
+			}
+			n /= 10;
+		}
+		return m;
 	}
 }
