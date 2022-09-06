14320b1a2037c070a9b2f72da9212fa1dd571cea
ssilantev <un.realgar@yandex.ru>
2022-09-06 08:51:13 +0700

    Turn off and store "For" class in Tmp

---
+++ src/ru.academIt.java/Tmp/For.java
@@ -0,0 +1,11 @@
+//package For;
+//
+//public class For {
+//	public static void dividedIntoFour(int[] s){
+//		for (int j = s.length - 1; j >= 1; j--) {
+//			if (s[j] % 4 == 0) {
+//				System.out.print(s[j] + " ");
+//			}
+//		}
+//	}
+//}
