4c33a5ad7bcbca33ddfcd46a7e7d65ab85bd9b73
ssilantev <un.realgar@yandex.ru>
2022-09-08 09:15:37 +0700

    Homework 4: For, fix #2

--- src/ru.academIt.java/For/MainFor.java
+++ src/ru.academIt.java/For/MainFor.java
@@ -2,13 +2,9 @@ package For;
 
 public class MainFor {
 	public static void main(String[] args) {
-		int[] Array = new int[101];
 		for (int i = 100; i >= 1; i--) {
-			Array[i] = i;
-		}
-		for (int j = Array.length - 1; j >= 1; j--) {
-			if (Array[j] % 4 == 0) {
-				System.out.print(Array[j] + " ");
+			if (i % 4 == 0) {
+				System.out.print(i + " ");
 			}
 		}
 	}
