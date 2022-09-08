cc1831a7605b4b6e63d9df86670eb775f7565076
ssilantev <un.realgar@yandex.ru>
2022-09-08 10:52:26 +0700

    Homework 4: For, fix #3

--- src/ru.academIt.java/For/MainFor.java
+++ src/ru.academIt.java/For/MainFor.java
@@ -1,11 +1,16 @@
 package For;
 
 public class MainFor {
+	private static void function(int i) {
+		if (i % 4 == 0) {
+			System.out.print(i + " ");
+		}
+	}
+
 	public static void main(String[] args) {
+
 		for (int i = 100; i >= 1; i--) {
-			if (i % 4 == 0) {
-				System.out.print(i + " ");
-			}
+			function(i);
 		}
 	}
 }
