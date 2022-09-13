f00ccfb0126b6a37a96cd6fc75b534c2c3da0d56
ssilantev <un.realgar@yandex.ru>
2022-09-13 08:58:40 +0700

    Homework 4: For, fix #5

--- src/ru.academIt.java/For/MainFor.java
+++ src/ru.academIt.java/For/MainFor.java
@@ -1,14 +1,14 @@
 package For;
 
 public class MainFor {
-	private static boolean function(int i) {
+	private static boolean divToFour(int i) {
 		return i % 4 == 0;
 	}
 
 	public static void main(String[] args) {
 
 		for (int i = 100; i >= 1; i--) {
-			if (function(i)) {
+			if (divToFour(i)) {
 				System.out.print(i + " ");
 			}
 		}
